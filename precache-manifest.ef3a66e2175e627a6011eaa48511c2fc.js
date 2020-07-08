02b1547aece441a194d6d4d21e3f9629a4c353de
nducanh255 <43198097+nducanh255@users.noreply.github.com>
2020-07-08 20:56:31 +0700

    Updates

---
+++ precache-manifest.ef3a66e2175e627a6011eaa48511c2fc.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "67b49781f3c19c7215ce",
+    "url": "/INT-2208-3-FINAL/static/js/app.de30f5a6.chunk.js"
+  },
+  {
+    "revision": "0017bb63ea3c03fd9345",
+    "url": "/INT-2208-3-FINAL/static/js/runtime~app.0a0b18f7.js"
+  },
+  {
+    "revision": "6823e85391106541dca6",
+    "url": "/INT-2208-3-FINAL/static/js/2.b52e1596.chunk.js"
+  },
+  {
+    "revision": "d5460cb73e9364ccfc47de0baecba170",
+    "url": "/INT-2208-3-FINAL/static/media/duck_04.d5460cb7.jpg"
+  },
+  {
+    "revision": "a1535693a3d05c2fcfa4f573b386072e",
+    "url": "/INT-2208-3-FINAL/static/media/duck_03.a1535693.jpg"
+  },
+  {
+    "revision": "11faa6616ad4132146a846925bd63239",
+    "url": "/INT-2208-3-FINAL/static/media/duck_02.11faa661.jpg"
+  },
+  {
+    "revision": "77d3235fff8f7b8d8410359652425d0c",
+    "url": "/INT-2208-3-FINAL/static/media/duck_05.77d3235f.jpg"
+  },
+  {
+    "revision": "1e0dfacee449186a76b306f59c78da1f",
+    "url": "/INT-2208-3-FINAL/static/media/duck_06.1e0dface.jpg"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/INT-2208-3-FINAL/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "ca57bf641a2f7e2405ec3addaab55ed9",
+    "url": "/INT-2208-3-FINAL/static/media/shapes_home_background.ca57bf64.jpg"
+  },
+  {
+    "revision": "599d99cbe8899e3305773390408fd14c",
+    "url": "/INT-2208-3-FINAL/static/media/duck_home_background.599d99cb.jpg"
+  },
+  {
+    "revision": "a605d8decb3ec6c50e401331ce2e923f",
+    "url": "/INT-2208-3-FINAL/static/media/duck_quack.a605d8de.mp3"
+  },
+  {
+    "revision": "bb390ee7d11e0aa08b92e2b75fa3797b",
+    "url": "/INT-2208-3-FINAL/static/media/fluffing_duck.bb390ee7.mp3"
+  },
+  {
+    "revision": "1da00e3001bc5305f33f41c7e2765c93",
+    "url": "/INT-2208-3-FINAL/static/media/background_ducks.1da00e30.jpg"
+  },
+  {
+    "revision": "2ff1bad9b29da0c8a1f3d4f5e7b6a597",
+    "url": "/INT-2208-3-FINAL/static/media/right.2ff1bad9.mp3"
+  },
+  {
+    "revision": "923edd42c60d4d074f016b6786300b3b",
+    "url": "/INT-2208-3-FINAL/static/media/wrong.923edd42.mp3"
+  },
+  {
+    "revision": "1c02462b4f52024f815d7a93a622c657",
+    "url": "/INT-2208-3-FINAL/static/media/streak.1c02462b.mp3"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/INT-2208-3-FINAL/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/INT-2208-3-FINAL/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/INT-2208-3-FINAL/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/INT-2208-3-FINAL/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/INT-2208-3-FINAL/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/INT-2208-3-FINAL/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/INT-2208-3-FINAL/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/INT-2208-3-FINAL/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/INT-2208-3-FINAL/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/INT-2208-3-FINAL/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/INT-2208-3-FINAL/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/INT-2208-3-FINAL/expo-service-worker.js"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/INT-2208-3-FINAL/favicon-16.png"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/INT-2208-3-FINAL/favicon-32.png"
+  },
+  {
+    "revision": "615280e83775685e49117729b227d0fc",
+    "url": "/INT-2208-3-FINAL/favicon.ico"
+  },
+  {
+    "revision": "8f85e1849cf2e96e65649a7dc33d3887",
+    "url": "/INT-2208-3-FINAL/index.html"
+  },
+  {
+    "revision": "3c448f2fddd12e670ba5cca903bfcda6",
+    "url": "/INT-2208-3-FINAL/manifest.json"
+  },
+  {
+    "revision": "e8250daf7084454b684a8ab5633b474a",
+    "url": "/INT-2208-3-FINAL/register-service-worker.js"
+  }
+];
